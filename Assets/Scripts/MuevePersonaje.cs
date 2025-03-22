dadf59fb9b0287a18d75089472a9053fd9f03f9e
TECcoding <142265349+TECcoding@users.noreply.github.com>
2025-03-22 15:56:24 -0600

    Movimiento de personaje y correcion de mapa

---
+++ Assets/Scripts/MuevePersonaje.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MuevePersonaje : MonoBehaviour
+{
+    // Velocidad
+
+    [SerializeField]
+    private float velocidadX;
+
+    [SerializeField] // Le doy permiso a Unity de acceder a la variable
+    private float velocidadY;
+
+    // RigidBody para usar la fisica
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    // FixUpdate is called 50 times per second
+    void FixedUpdate()
+    {
+       float movHorizontal = Input.GetAxis("Horizontal");
+       float movVertical = Input.GetAxis("Vertical");
+
+       if (movVertical > 0) {
+        rb.linearVelocity = new Vector2(movHorizontal*velocidadX, movVertical*velocidadY); 
+       } else {
+        rb.linearVelocity = new Vector2(movHorizontal*velocidadX, rb.linearVelocityY); 
+       }
+       
+    }
+
+}
